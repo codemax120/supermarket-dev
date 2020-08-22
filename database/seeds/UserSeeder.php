1d0047f1c66ab4978fc1ba52af3499fde2942c98
Max <hh@smt.gt>
2020-08-22 17:11:45 -0600

    Users Resource
    In this commit the entire development module for users is added, however validations for roles and permissions have not yet been related

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'first_name' => 'admin',
+            'last_name' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('admin'),
+            'remember_token' => Str::random(10),
+        ]);
+        factory(User::class, 20)->create();
+    }
+}
